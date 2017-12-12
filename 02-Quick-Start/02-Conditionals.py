89614517a27b6e63b8fd84dd722884ecc822fda2
Viking Den <vikingden7@gmail.com>
2017-12-12 22:04:50 +0800

    add Conditional.py

---
+++ 02-Quick-Start/02-Conditionals.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+a, b = 5, 1
+
+if a < b:
+    print("a ({}) is less than b ({}).".format(a, b))
+else:
+    print("a ({}) is not less than b ({}).".format(a, b))
+
+print("foo" if a < b else "bar")
