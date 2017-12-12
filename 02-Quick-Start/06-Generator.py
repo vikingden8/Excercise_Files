926198f410448fcf23e10d20fa9e2e0a32fda6ce
Viking Den <vikingden7@gmail.com>
2017-12-12 22:31:14 +0800

    add generator

--- 02-Quick-Start/06-Generator.py
+++ 02-Quick-Start/06-Generator.py
@@ -3,16 +3,22 @@
 
 def isprime(n):
     if n == 1:
-        print('1 is special.')
         return False
     for x in range(2, n):
         if n % x == 0:
-            print('{} equals {} x {}'.format(n, x, n//x))
             return False
     else:
-        print(n, ' is prime.')
         return True
 
 
-for n in range(1, 50):
-    isprime(n)
+def primes(n=1):
+    while True:
+        if isprime(n):
+            yield n
+        n += 1
+
+
+for n in primes():
+    if n > 100:
+        break
+    print(n)
