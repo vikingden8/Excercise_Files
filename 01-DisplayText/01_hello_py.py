e987b4f72308b949358dc61e4474ece52ff53907
Viking Den <vikingden7@gmail.com>
2018-01-08 22:45:51 +0800

    add display text

---
+++ 01-DisplayText/01_hello_py.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def main():
+    print('This is a text with single quote')
+    print("This is a text with double quote")
+    print("This ia a text within a double quote:\"")
+    print("This is a text" + "with a plus expression.")
+    print("""
+    This is a triple quote demo
+    we can have multi line of content like this
+    like that
+    """)
+
+if __name__ == '__main__':
+    main()
